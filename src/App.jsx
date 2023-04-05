bd4da084d0816f45326c4ac54a9e5e9c99e5ad69
DaomBae <da6970@gmail.com>
2023-04-05 14:28:26 +0900

    LocalStorage + parseInt + useEffect + State + if/get

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import GameBoard from "./components/GameBoard";
 import GameResault from "./components/GameResult";
 
+//시작할때  point 초기값을 0으로 아예 설정
 function App() {
+  useEffect(()=>{
+    let savedPoint = localStorage.getItem("point");
+    if (!savedPoint) {
+      localStorage.setItem("point",0)
+    }
+  },[]);
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <GameResault/>
--- src/components/GameResult.jsx
+++ src/components/GameResult.jsx
@@ -1,5 +1,13 @@
 const GameResult = () => {
-    return <div className="bg-red-100 w-full grow">게임결과</div>;
+    // | 은  null, if문과 비슷한 역할을 합니다
+    const point = localStorage.getItem("point") | 0;
+  
+    return (
+      <div className="bg-purple-300 w-full grow flex flex-col justify-end items-center text-white pb-8 shadow-pink-300 shadow-lg">
+        <div className="text-8xl font-black">Up & Down</div>
+        <div className="text-2xl">현재 점수 : {point}</div>
+      </div>
+    );
   };
   
   export default GameResult;
